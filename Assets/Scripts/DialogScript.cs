ceaa7654d963958472a33cf42bf8d91b7f3632dd
Lily Djami <mlily_d@proton.me>
2023-07-08 18:09:46 +0200

    replace text in dialogbox?

--- Assets/Scripts/DialogScript.cs
+++ Assets/Scripts/DialogScript.cs
@@ -9,8 +9,11 @@ public class DialogScript : MonoBehaviour
     public static int TextSpeed = 2;
 
     public TMP_Text Text;
-    private string CurrentText; 
-    private string FullText = "You are the regional district manager of the local evil horde. Your boss wants you to deal with some pesty little heroes by sending the troops in their direction. It is a simple desk job. ";
+    private string currentText; 
+
+    private string firstText = "You are the regional district manager of the local evil horde. Your boss wants you to deal with some pesty little heroes by sending the troops in their direction. It is a simple desk job. ";
+    private string secondText = "The available troops come in, zou send them out in a way that makes sense and kills the hero. Easy. But damn! They kinda cute???? Maybe just send some easier monsters to scare them off but not kill them?" ;
+    private string thirdText = "Oh no they are still going… Thez look a little hungry though, maybe you should send them a snack? The boss won’t like that though… Guess you have to smuggle it to them using the horde. Hopefully they survive your romantic gift.";
     
     //CanvasGroup Group;
     
@@ -19,40 +22,42 @@ public class DialogScript : MonoBehaviour
     {
         //Group = GetComponent<CanvasGroup>();
         //Group.alpha = 0;        
-        Show(FullText);
+        Show(firstText);
     }
 
    public void Show(string text)
    {    
         //Group.alpha = 1;
-        CurrentText = text;
+        currentText = text;
         StartCoroutine(DisplayText());
    }
 
 
    //idk if i need this but maybe?
    public void ReplaceText(string text)
-   {
+   {    
         StopAllCoroutines();
         Text.text = "";
-        FullText = text;
+        currentText = text;
         Show(text);
    }
 
+    //didn't actually test this yet 
     public void Close()
     {
         //Anim?.SetBool("Open", false);\
         //Group.alpha = 0;
-        CurrentText = "";
+        currentText = "";
         StopAllCoroutines();
         
     }
 
+    // the thing that makes the text scroll
     private IEnumerator DisplayText()
     {
         Text.text = "";
 
-        foreach(char c in CurrentText.ToCharArray())
+        foreach(char c in currentText.ToCharArray())
         {   
             Text.text += c;
 
@@ -62,16 +67,24 @@ public class DialogScript : MonoBehaviour
     }
 
     void Update()
-    {
+    {   
         if (Input.GetMouseButtonDown(0))
         {   
             StopAllCoroutines();
-            Text.text = FullText;
+            Text.text = currentText;
+            yield return new WaitForSeconds(0.2f);
             Debug.Log("Pressed left-click.");
         }
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Pressed right-click.");
+        }
+        //yield return new WaitForSeconds(0.2f);
+        
+        if (Text.text == firstText){
+            ReplaceText(secondText);
+        } else if (Text.text == secondText){
+            ReplaceText(thirdText);
         }
     }
 }
