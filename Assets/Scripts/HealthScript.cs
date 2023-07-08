c6af36638699bbc32ec49cdf546c329bc93c3e17
Mihai Verzan <rtyh12@gmail.com>
2023-07-08 12:39:56 +0300

    Add healthbars

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,14 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthScript : MonoBehaviour
 {
-    public float health;
+    private float _health;
+    public float health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+            UpdateHealthBar(_health);
+        }
+    }
     public float maxHealth;
+    
+    public GameObject healthBarPrefab;
+    private Image healthBarImage;
+    private GameObject healthBarCanvas;
+
+    public void UpdateHealthBar(float health)
+    {
+        if (healthBarImage == null)
+        {
+            return;
+        }
+        healthBarImage.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1f);
+        healthBarCanvas.SetActive(health < maxHealth - 0.01);
+    }
 
     void Start()
     {
+        healthBarCanvas = Instantiate(healthBarPrefab, transform.position, Quaternion.identity);
+        healthBarCanvas.transform.SetParent(this.transform);
+        healthBarImage = healthBarCanvas.transform
+                        .GetChild(0).transform
+                        .GetChild(0).gameObject
+                        .GetComponent<Image>();
         health = maxHealth;
     }
 
