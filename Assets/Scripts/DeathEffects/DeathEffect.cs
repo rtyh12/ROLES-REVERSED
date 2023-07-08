d6adeba2d037ac2d8c1eb24d319b3f390d0348ea
Mihai Verzan <rtyh12@gmail.com>
2023-07-08 17:35:45 +0300

    Add flower and banona death effects

--- Assets/Scripts/DeathEffects/DeathEffect.cs
+++ Assets/Scripts/DeathEffects/DeathEffect.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public interface DeathEffect {
+public interface IDeathEffect {
     void PerformOn(GameObject hero);
 }
--- Assets/Scripts/DeathEffects/FlowerDeathEffect.cs
+++ Assets/Scripts/DeathEffects/FlowerDeathEffect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class FlowerDeathEffect : MonoBehaviour, DeathEffect {
+public class FlowerDeathEffect : MonoBehaviour, IDeathEffect {
     public float romanceAmount = 10;
     
     public void PerformOn(GameObject hero) {
